e9ca34ad0b5703feea22f2fad628f16db4ce0874
Naftali Kulik <nkulik1994@gmail.com>
2022-07-19 09:07:15 -0400

    commit tree 5b98585804cbe0b1f092183ac2a8827c3c4ab424

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,24 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :check_clickbait
+
+    # def check_clickbait
+    #     if title
+    #         unless title.match?(/Won't Believe/) || title.match?(/Secret/) || title.match?(/Guess/) || (title.match?(/Top/) && title.match?(/[1-9]/))
+    #             errors.add(:title, 'sorry dude')
+    #         end
+    #     end
+    # end
+
+    def check_clickbait
+        required = ["Won't Believe", "Secret", "Guess"]
+        if title
+            unless required.find { |str| title.include?(str) }
+                errors.add(:title, 'sorry dude')
+            end
+        end
+    end
 end
